7437ed3697ea73e9cbfc15c36daa92ae77a57e81
MayuriFokmare <41722994+MayuriFokmare@users.noreply.github.com>
2025-01-05 06:44:31 +0000

    Create config.py
    added aws db configuration

---
+++ ev-system-backend/config/config.py
@@ -0,0 +1,7 @@
+# Database configuration
+db_config = {
+    "host": "evsystem.c18ascuy0vdp.ap-south-1.rds.amazonaws.com",
+    "user": "admin",
+    "password": "Nilmay2312123",
+    "database": "evsystem" # Replace with your actual database name
+}
