ad946adb40080e3e984c76fdb229fd17f5a13f8a
MayuriFokmare <41722994+MayuriFokmare@users.noreply.github.com>
2025-08-04 14:43:59 +0100

    Update config.py

--- ev-system-backend/config/config.py
+++ ev-system-backend/config/config.py
@@ -3,5 +3,5 @@ db_config = {
     "host": "evsystem.c18ascuy0vdp.ap-south-1.rds.amazonaws.com",
     "user": "admin",
     "password": "Nilmay2312123",
-    "database": "evsystem" # Replace with your actual database name
+    "database": "evsystem"
 }
